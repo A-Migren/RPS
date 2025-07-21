599cd5d9e16bfe97fa18b31c28022a5c0e557ef8
A-Migren <sashaefits@gmail.com>
2025-07-21 19:26:22 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args)  {
+        Random random = new Random();
+        int p = random.nextInt(3);
+        int v = random.nextInt(3);
+        String[] choices = {"Камень", "Ножницы", "Бумагу"};
+        String schoiceNamep = choices[p];
+        String schoiceNamev = choices[v];
+
+        System.out.println("Петя выкинул " + schoiceNamep);
+        System.out.println("Вася выкинул " + schoiceNamev);
+
+        if (p == v) {
+            System.out.println("Ничья");
+        } else if ((p == 0 && v == 1)||(p == 1 && v == 2)||(p == 2 && v == 0)){
+            System.out.println("Петя победил!");
+        } else {
+            System.out.println("Вася победил!");
+        }
+    }
+}
